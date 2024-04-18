0b792acd70b1a1e077d5c7ee6bf9173a78486d2a
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-04-18 14:17:12 +0000

    approval security

--- src/commands/token/approval.ts
+++ src/commands/token/approval.ts
@@ -1,15 +1,19 @@
-import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import {
+  CommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
 import axios from "axios";
 import networks, { translateNetwork } from "../../utils/networks";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("checkmalicious")
-    .setDescription("Check if am address is marked malicious.")
+    .setName("approvalsecurity")
+    .setDescription("Check if the approval is secure.")
     .addStringOption((opt) =>
       opt
         .setName("address")
-        .setDescription("Address to lookup for.")
+        .setDescription("Contract address to lookup for.")
         .setRequired(true)
     )
     .addStringOption((opt) =>
@@ -34,7 +38,7 @@ export default {
 
     const response = await axios
       .get(
-        `https://api.gopluslabs.io/api/v1/address_security/${a}?chain_id=${translated}`
+        `https://api.gopluslabs.io/api/v1/approval_security/${translated}?contract_addresses=${a}`
       )
       .then((d) => d.data)
       .catch(() =>
@@ -45,16 +49,30 @@ export default {
       );
 
     if (response.result) {
-      const fields: {name: string, value: string, inline: boolean}[] = new Array();
+      //console.log(response);
+      const fields: { name: string; value: string; inline: boolean }[] =
+        new Array();
 
       for (const [key, value] of Object.entries(response.result)) {
-        const fieldValue = value === "1" ? "✅ Yes" : "❌ No";
-        
+        const fieldValue = value === "1" || 1 ? "✅ Yes" : "❌ No";
+
         fields.push({ name: key.charAt(0).toUpperCase()+key.slice(1).replaceAll("_", " "), value: fieldValue, inline: true });
       }
 
-      const embed = new EmbedBuilder().setTitle("Malicious Address Lookup").setDescription(`**Network**: ${networks.find(net => net.id == translated)?.name} (${translated})`).addFields({ name: "Address", value: `\`${a}\``, inline: false }, ...fields).setColor("Blurple");
-      return interaction.reply({ embeds: [embed]});
+      if (response.result && response.result.risky_approval && response.result.risky_approval.risk) {
+        fields.push({ name: "Risk", value: `${response.result.risky_approval.risk}`, inline: false})
+      }
+
+      const embed = new EmbedBuilder()
+        .setTitle("Contract Approval Security")
+        .setDescription(
+          `**Network**: ${
+            networks.find((net) => net.id == translated)?.name
+          } (${translated})`
+        )
+        .addFields({ name: "CA", value: `\`${a}\``, inline: false }, ...fields)
+        .setColor("Blurple");
+      return interaction.reply({ embeds: [embed] });
     } else {
       return interaction.reply({
         content: "Error looking up address",
