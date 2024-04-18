275d241c35fa47eedf7327b55e4cb43317e0f105
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-04-18 16:19:24 +0000

    phishing detector

---
+++ src/commands/external/phishing.ts
@@ -0,0 +1,36 @@
+import {
+  CommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js";
+import axios from "axios";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("phishing")
+    .setDescription("See if a website is a phishing link.")
+    .addStringOption((opt) =>
+      opt.setName("link").setDescription("Website to lookup.").setRequired(true)
+    ),
+  execute: async (interaction: CommandInteraction) => {
+    const link = interaction.options.get("link")?.value?.toString();
+
+    const response = await axios
+      .get(`https://api.gopluslabs.io/api/v1/phishing_site/?url=${link}`)
+      .then((d) => d.data)
+      .catch(() =>
+        interaction.reply({
+          content: "Error looking up website",
+          ephemeral: true,
+        })
+      );
+
+      console.log(response);
+
+    const embed = new EmbedBuilder()
+      .setTitle("Phishing Detector")
+      .setDescription(`${(response.result.phishing_site == 1) ? "⚠️ :no_entry: This website is a **Phishing Site**, please refrain from using it!!" : "Passed test, we have no guarantee the site is still safe tho."}`)
+      .setColor("Blurple");
+    return interaction.reply({ embeds: [embed] });
+  },
+};
